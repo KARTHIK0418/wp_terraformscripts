5ffceab10ccd79fa3d52dfa4f385e65f431324e2
KARTHIK0418 <147519514+KARTHIK0418@users.noreply.github.com>
2024-02-19 15:55:08 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 #Creating Wordpress subnet
 resource "aws_subnet" "wp_subnet-1" {
   vpc_id                  = aws_vpc.demovpc.id
-  cidr_block              = var.subnet1_cidr
+  cidr_blocks             = ["10.0.1.0/24"]
   map_public_ip_on_launch = true
   availability_zone       = "us-east-2a"
   tags = {
